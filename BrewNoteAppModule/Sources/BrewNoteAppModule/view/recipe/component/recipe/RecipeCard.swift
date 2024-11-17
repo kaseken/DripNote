5a96bf78bed89a4839e5fe36e04067f55992cea5
kaseken <k.kaseda0925@gmail.com>
2024-11-17 23:35:27 +0900

    feat: Implement RecipeCard

--- BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeCard.swift
+++ BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeCard.swift
@@ -1,7 +1,55 @@
 import SwiftUI
 
 struct RecipeCard: View {
+    let test: String
+    let timerLabel: String
+    let timerProgress: Double
+
     var body: some View {
-        Text("RecipeCard")
+        HStack(alignment: .center, spacing: 8.0) {
+            RecipeTimer(
+                text: timerLabel,
+                size: 64.0,
+                progress: timerProgress
+            )
+            .padding(16.0)
+            // TODO: Define typography.
+            Text(test)
+                .font(.system(size: 16.0))
+                .fontWeight(.regular)
+                .foregroundStyle(Color.grayText)
+                .lineSpacing(4.0)
+            Spacer()
+        }
+        .frame(minHeight: 96)
+        .frame(width: .infinity)
+        .background(Color.backgroundWhite)
+        .cornerRadius(8.0)
     }
+}
+
+#Preview {
+    VStack(spacing: 16.0) {
+        Spacer()
+        RecipeCard(
+            test: "20秒間かけて\n湯を40g注ぎましょう",
+            timerLabel: "-",
+            timerProgress: 20.0 / 20.0
+        )
+        .padding(.horizontal, 16.0)
+        RecipeCard(
+            test: "20秒間待ちましょう",
+            timerLabel: "16",
+            timerProgress: 4.0 / 20.0
+        )
+        .padding(.horizontal, 16.0)
+        RecipeCard(
+            test: "全量が100gになるまで\n20秒間かけて\n湯を60g注ぎましょう",
+            timerLabel: "20",
+            timerProgress: 0 / 20.0
+        )
+        .padding(.horizontal, 16.0)
+        Spacer()
+    }
+    .background(Color.backgroundBeige)
 }
--- BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeTimer.swift
+++ BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeTimer.swift
@@ -5,15 +5,15 @@ struct RecipeTimer: View {
 
     let text: String
     let size: CGFloat
-    let activeRatio: Double
+    let progress: Double
 
     init(text: String,
          size: CGFloat,
-         activeRatio: Double = 1.0)
+         progress: Double = 1.0)
     {
         self.text = text
         self.size = size
-        self.activeRatio = activeRatio
+        self.progress = min(max(progress, 0.0), 1.0)
     }
 
     var body: some View {
@@ -25,7 +25,7 @@ struct RecipeTimer: View {
                 )
                 .frame(width: size, height: size)
             Circle()
-                .trim(from: 0.0, to: activeRatio)
+                .trim(from: 0.0, to: progress)
                 .stroke(
                     Color.themeGray,
                     lineWidth: Self.barWidth
@@ -34,6 +34,7 @@ struct RecipeTimer: View {
                 .rotationEffect(.degrees(-90))
             // TODO: Define typography.
             Text(text)
+                .font(.system(size: 16.0))
                 .foregroundStyle(Color.grayText)
                 .fontWeight(.bold)
         }
@@ -43,9 +44,9 @@ struct RecipeTimer: View {
 
 #Preview {
     VStack(spacing: 16.0) {
-        RecipeTimer(text: "20", size: 64.0, activeRatio: 0.0)
-        RecipeTimer(text: "20", size: 64.0, activeRatio: 0.2)
-        RecipeTimer(text: "20", size: 64.0, activeRatio: 0.8)
-        RecipeTimer(text: "20", size: 64.0, activeRatio: 1.0)
+        RecipeTimer(text: "20", size: 64.0, progress: 0.0)
+        RecipeTimer(text: "20", size: 64.0, progress: 0.2)
+        RecipeTimer(text: "20", size: 64.0, progress: 0.8)
+        RecipeTimer(text: "20", size: 64.0, progress: 1.0)
     }
 }
