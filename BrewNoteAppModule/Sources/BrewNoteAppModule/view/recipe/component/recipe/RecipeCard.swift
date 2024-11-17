e0630436b372b4ccdde991430edca8e6655cbaef
kaseken <k.kaseda0925@gmail.com>
2024-11-17 23:47:58 +0900

    fix: Adjust card style

--- BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeCard.swift
+++ BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeCard.swift
@@ -28,8 +28,20 @@ struct RecipeCard: View {
     }
 }
 
+struct RecipeCardConnector: View {
+    let color: SwiftUI.Color
+    let width: CGFloat
+    let height: CGFloat
+
+    var body: some View {
+        Rectangle()
+            .fill(color)
+            .frame(width: width, height: height)
+    }
+}
+
 #Preview {
-    VStack(spacing: 16.0) {
+    VStack(spacing: 0) {
         Spacer()
         RecipeCard(
             test: "20秒間かけて\n湯を40g注ぎましょう",
@@ -37,12 +49,22 @@ struct RecipeCard: View {
             timerProgress: 20.0 / 20.0
         )
         .padding(.horizontal, 16.0)
+        RecipeCardConnector(
+            color: Color.themeBeigeLight,
+            width: 12.0,
+            height: 16.0
+        )
         RecipeCard(
             test: "20秒間待ちましょう",
             timerLabel: "16",
             timerProgress: 4.0 / 20.0
         )
         .padding(.horizontal, 16.0)
+        RecipeCardConnector(
+            color: Color.themeBeigeLight,
+            width: 12.0,
+            height: 16.0
+        )
         RecipeCard(
             test: "全量が100gになるまで\n20秒間かけて\n湯を60g注ぎましょう",
             timerLabel: "20",
