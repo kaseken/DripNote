b6453223c8a283d1dec719e7bd3d5c53cdb1c811
kaseken <k.kaseda0925@gmail.com>
2024-11-17 23:09:51 +0900

    feat: Implement RecipeTimer

--- BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeTimer.swift
+++ BrewNoteAppModule/Sources/BrewNoteAppModule/view/recipe/component/recipe/RecipeTimer.swift
@@ -1,14 +1,37 @@
 import SwiftUI
 
 struct RecipeTimer: View {
+    private static let barWidth: CGFloat = 8.0
+
     let text: String
     let size: CGFloat
+    let activeRatio: Double
+
+    init(text: String,
+         size: CGFloat,
+         activeRatio: Double = 1.0)
+    {
+        self.text = text
+        self.size = size
+        self.activeRatio = activeRatio
+    }
 
     var body: some View {
         ZStack(alignment: .center) {
             Circle()
-                .stroke(Color.themeGray, lineWidth: 8.0)
+                .stroke(
+                    Color.themeNavyDark,
+                    lineWidth: Self.barWidth
+                )
                 .frame(width: size, height: size)
+            Circle()
+                .trim(from: 0.0, to: activeRatio)
+                .stroke(
+                    Color.themeGray,
+                    lineWidth: Self.barWidth
+                )
+                .frame(width: size, height: size)
+                .rotationEffect(.degrees(-90))
             // TODO: Define typography.
             Text(text)
                 .foregroundStyle(Color.grayText)
@@ -19,5 +42,10 @@ struct RecipeTimer: View {
 }
 
 #Preview {
-    RecipeTimer(text: "20", size: 64.0)
+    VStack(spacing: 16.0) {
+        RecipeTimer(text: "20", size: 64.0, activeRatio: 0.0)
+        RecipeTimer(text: "20", size: 64.0, activeRatio: 0.2)
+        RecipeTimer(text: "20", size: 64.0, activeRatio: 0.8)
+        RecipeTimer(text: "20", size: 64.0, activeRatio: 1.0)
+    }
 }
