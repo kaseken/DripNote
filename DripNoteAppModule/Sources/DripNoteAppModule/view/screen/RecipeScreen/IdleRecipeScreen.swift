6833bda79ac55a00a0d2a7947ab5d3b52a92d9b6
kaseken <k.kaseda0925@gmail.com>
2024-11-24 02:52:09 +0900

    fix: Adjust style

--- DripNoteAppModule/Sources/DripNoteAppModule/view/screen/RecipeScreen/IdleRecipeScreen.swift
+++ DripNoteAppModule/Sources/DripNoteAppModule/view/screen/RecipeScreen/IdleRecipeScreen.swift
@@ -25,9 +25,13 @@ struct IdleRecipeScreen: View {
                 }
             }
             ScrollView {
-                ForEach(RecipeConverter.toRecipeCardStates(recipe: recipe)) { recipeCardState in
-                    RecipeCard(state: recipeCardState)
+                VStack(spacing: 0) {
+                    ForEach(RecipeConverter.toRecipeCardStates(recipe: recipe)) { recipeCardState in
+                        RecipeCard(state: recipeCardState)
+                    }
                 }
+                .padding(.horizontal, 16.0)
+                .padding(.vertical, 16.0)
             }
         }
         .background(Color.backgroundBeige)
