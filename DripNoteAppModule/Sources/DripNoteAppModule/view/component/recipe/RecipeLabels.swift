28a336eabb98d829d6e5bb24f41a66a2b62cb826
kaseken <k.kaseda0925@gmail.com>
2024-11-23 23:23:24 +0900

    fix: Add label

--- DripNoteAppModule/Sources/DripNoteAppModule/view/component/recipe/RecipeLabels.swift
+++ DripNoteAppModule/Sources/DripNoteAppModule/view/component/recipe/RecipeLabels.swift
@@ -9,6 +9,15 @@ enum RecipeLabels {
         )
     }
 
+    static func stepPour(until: Gram, water: Gram, seconds: Second) -> String {
+        String(
+            format: String(localized: "step_pour_until", bundle: Bundle.module),
+            water.value,
+            seconds.value,
+            until.value
+        )
+    }
+
     static func stepWait(seconds: Second) -> String {
         String(
             format: String(localized: "step_wait", bundle: Bundle.module),
--- DripNoteAppModule/Tests/DripNoteAppModuleTest/view/recipe/RecipeLabelsTest.swift
+++ DripNoteAppModule/Tests/DripNoteAppModuleTest/view/recipe/RecipeLabelsTest.swift
@@ -8,6 +8,14 @@ struct RecipeLabelsTest {
             "Pour 60g of hot water in 20 seconds.")
         #expect(RecipeLabels.stepPour(water: Gram(60), seconds: Second(1)) ==
             "Pour 60g of hot water in 1 second.")
+        #expect(RecipeLabels.stepPour(until: Gram(100),
+                                      water: Gram(60),
+                                      seconds: Second(20)) ==
+                "Pour 60g of hot water in 20 seconds until the total amount reaches 100g.")
+        #expect(RecipeLabels.stepPour(until: Gram(100),
+                                      water: Gram(60),
+                                      seconds: Second(1)) ==
+                "Pour 60g of hot water in 1 second until the total amount reaches 100g.")
     }
 
     @Test
