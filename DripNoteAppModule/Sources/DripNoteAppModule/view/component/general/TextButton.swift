8241cce9ae5d1af5cb91fc1a2f56837c39099a3e
kaseken <k.kaseda0925@gmail.com>
2024-11-28 00:24:17 +0900

    feat: Add TextButton

---
+++ DripNoteAppModule/Sources/DripNoteAppModule/view/component/general/TextButton.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+enum TextButtonStyle {
+    case primary
+    case secondary
+}
+
+struct TextButton: View {
+    let label: String
+    let style: TextButtonStyle
+    let action: (() -> Void)?
+
+    private var textColor: SwiftUI.Color {
+        if action == nil {
+            return Color.white
+        }
+        return style == .primary ? Color.white : Color.themeNavyDark
+    }
+
+    private var backgroundColor: SwiftUI.Color {
+        if action == nil {
+            return Color.themeGrayLight
+        }
+        return style == .primary ? Color.themeNavyDark : Color.white
+    }
+
+    private var borderColor: SwiftUI.Color {
+        if action == nil {
+            return SwiftUI.Color.clear
+        }
+        return style == .primary ? SwiftUI.Color.clear : Color.themeNavyDark
+    }
+
+    var body: some View {
+        Button(action: action ?? {}) {
+            Text(label)
+                .font(.headline)
+                .foregroundColor(textColor)
+                .padding(.vertical, 12.0)
+                .padding(.horizontal, 24.0)
+                .background(backgroundColor)
+                .cornerRadius(16)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(borderColor, lineWidth: 1)
+                }
+        }
+        .disabled(action == nil)
+    }
+}
+
+#Preview {
+    TextButton(label: "ドリップ開始", style: .primary, action: {})
+    TextButton(label: "ドリップ開始", style: .primary, action: nil)
+    TextButton(label: "ドリップ終了", style: .secondary, action: {})
+    TextButton(label: "ドリップ終了", style: .secondary, action: nil)
+}
